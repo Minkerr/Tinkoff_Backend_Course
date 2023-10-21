589568e152449ab2cb4b41f2b520f7b95ae5916b
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-10-21 15:41:15 +0300

    first project1 commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
-public class Main {
+class Main {
+    private Main() {
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
