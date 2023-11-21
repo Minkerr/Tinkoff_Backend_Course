63243a5f9e6ee34433259ef0e894ae48c381a7c5
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-11-21 19:03:33 +0300

    add first two tasks

---
+++ src/test/java/edu/hw7/Task1Test.java
@@ -0,0 +1,27 @@
+package edu.hw7;
+
+import org.junit.jupiter.api.Test;
+import static edu.hw7.Task1.parallelIncrementor;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Task1Test {
+    @Test
+    void parallelIncrementor_shouldAddBigNumbers() {
+        //arrange
+        long exp = 1000001;
+        //act
+        var act = parallelIncrementor(1, 1000000);
+        //assert
+        assertThat(act).isEqualTo(exp);
+    }
+
+    @Test
+    void parallelIncrementor_shouldAddVeryBigAndOddNumbers() {
+        //arrange
+        long exp = 1000000000;
+        //act
+        var act = parallelIncrementor(1, 999999999);
+        //assert
+        assertThat(act).isEqualTo(exp);
+    }
+}
