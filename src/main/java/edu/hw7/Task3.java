93d274bd86b12713ec76f429fcdc5757b436ac10
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-11-24 21:09:51 +0300

    added Task3

---
+++ src/main/java/edu/hw7/Task3.java
@@ -0,0 +1,54 @@
+package edu.hw7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task3 {
+
+    private Task3() {
+    }
+
+    public record Person(int id, String name, String address, String phoneNumber) {
+    }
+
+    static class PersonDatabase {
+        PersonDatabase() {
+            phoneMap = new HashMap<>();
+            addressMap = new HashMap<>();
+            nameMap = new HashMap<>();
+            idMap = new HashMap<>();
+        }
+
+        private static Map<String, Person> phoneMap;
+        private static Map<String, Person> addressMap;
+        private static Map<String, Person> nameMap;
+        private static Map<Integer, Person> idMap;
+
+        public synchronized void add(Person person) {
+            phoneMap.put(person.phoneNumber, person);
+            nameMap.put(person.name, person);
+            addressMap.put(person.address, person);
+            idMap.put(person.id, person);
+        }
+
+        public synchronized void delete(int id) {
+            Person person = idMap.get(id);
+            idMap.remove(person.id);
+            addressMap.remove(person.address);
+            nameMap.remove(person.name);
+            phoneMap.remove(person.phoneNumber);
+        }
+
+        public synchronized Person findByName(String name) {
+            return nameMap.get(name);
+        }
+
+        public synchronized Person findByAddress(String address) {
+            return addressMap.get(address);
+        }
+
+        public synchronized Person findByPhone(String phone) {
+            return phoneMap.get(phone);
+        }
+    }
+}
