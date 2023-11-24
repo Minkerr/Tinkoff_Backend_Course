a7e8cec1bcdd232b8ae1997e80bc167f62bca8b8
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-11-24 23:21:46 +0300

    try to build 3.5

--- src/main/java/edu/hw7/Task3.java
+++ src/main/java/edu/hw7/Task3.java
@@ -51,4 +51,45 @@ public class Task3 {
             return phoneMap.get(phone);
         }
     }
+
+    static class PersonDatabaseNotSynchronized {
+        PersonDatabaseNotSynchronized() {
+            phoneMap = new HashMap<>();
+            addressMap = new HashMap<>();
+            nameMap = new HashMap<>();
+            idMap = new HashMap<>();
+        }
+
+        private static Map<String, Person> phoneMap;
+        private static Map<String, Person> addressMap;
+        private static Map<String, Person> nameMap;
+        private static Map<Integer, Person> idMap;
+
+        public void add(Person person) {
+            phoneMap.put(person.phoneNumber, person);
+            nameMap.put(person.name, person);
+            addressMap.put(person.address, person);
+            idMap.put(person.id, person);
+        }
+
+        public void delete(int id) {
+            Person person = idMap.get(id);
+            idMap.remove(person.id);
+            addressMap.remove(person.address);
+            nameMap.remove(person.name);
+            phoneMap.remove(person.phoneNumber);
+        }
+
+        public Person findByName(String name) {
+            return nameMap.get(name);
+        }
+
+        public Person findByAddress(String address) {
+            return addressMap.get(address);
+        }
+
+        public Person findByPhone(String phone) {
+            return phoneMap.get(phone);
+        }
+    }
 }
--- src/test/java/edu/hw7/Task3Test.java
+++ src/test/java/edu/hw7/Task3Test.java
@@ -5,6 +5,10 @@ import edu.hw7.Task3.PersonDatabase;
 import org.junit.jupiter.api.Test;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import static edu.hw7.Task3.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task3Test {
@@ -28,7 +32,8 @@ public class Task3Test {
                     Person byAddress = db.findByName(address);
                     Person byPhone = db.findByName(phone);
 
-                    assertThat(byName.equals(byAddress) && byName.equals(byPhone)).isTrue();
+                    assertThat(byName == null && byAddress == null && byPhone == null ||
+                        byName.equals(byAddress) && byName.equals(byPhone)).isTrue();
                 }
             );
 
@@ -60,6 +65,41 @@ public class Task3Test {
                     Person byName = db.findByName(name);
                     Person byAddress = db.findByName(address);
                     Person byPhone = db.findByName(phone);
+
+                    assertThat(byName == null && byAddress == null && byPhone == null ||
+                        byName.equals(byAddress) && byName.equals(byPhone)).isTrue();
+                }
+            );
+
+        }
+        executor.shutdown();
+    }
+
+    @Test
+    void findBy_shouldWorkWithReadWriteLock() {
+        // arrange
+        PersonDatabaseNotSynchronized db = new PersonDatabaseNotSynchronized();
+        ExecutorService executor = Executors.newFixedThreadPool(8);
+        ReadWriteLock lock = new ReentrantReadWriteLock();
+        for (int i = 10000; i < 99999; i++) {
+            Person person = new Person(i, "name" + i, "address" + i, "phone" + i);
+            executor.execute(() -> {
+                lock.writeLock().lock();
+                db.add(person);
+                lock.writeLock().unlock();
+            });
+        }
+        // act
+        for (int i = 0; i < 99999; i++) {
+            String name = "name" + i;
+            String address = "address" + i;
+            String phone = "phone" + i;
+            executor.execute(() -> {
+                    lock.readLock().lock();
+                    Person byName = db.findByName(name);
+                    Person byAddress = db.findByName(address);
+                    Person byPhone = db.findByName(phone);
+                    lock.readLock().unlock();
 
                     assertThat(byName == null && byAddress == null && byPhone == null ||
                         byName.equals(byAddress) && byName.equals(byPhone)).isTrue();
