70d1d768bb6fda534d4b559e1050f168a72979f7
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-04 00:55:56 +0300

    try to execute task3

--- src/main/java/edu/hw8/Task3/Task3.java
+++ src/main/java/edu/hw8/Task3/Task3.java
@@ -1,36 +1,37 @@
 package edu.hw8.Task3;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class Task3 {
     private Task3() {
     }
 
-    private static final int MAX_PASSWORD_LENGTH = 5;
-    private static final int QUEUE_CAPACITY = 1000000;
-    private static final char[] ALPHABET = "abcdefghijklmnopqrstuvwxyz0123456789".toCharArray();
+    private static final int MAX_PASSWORD_LENGTH = 6;
+    private static final int THREAD_NUMBER = 8;
+    private static final String START_PASSWORD = "999";
 
     @SuppressWarnings("MagicNumber")
-    public static String stringToMD5(String s) {
+    private static String stringToMD5(String s) {
         try {
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            byte[] messageDigest = md.digest(s.getBytes());
-            StringBuffer hexString = new StringBuffer();
-
-            for (int i = 0; i < messageDigest.length; i++) {
-                String hex = Integer.toHexString(0xff & messageDigest[i]);
-                if (hex.length() == 1) {
-                    hexString.append('0');
+            try {
+                java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+                byte[] array = md.digest(s.getBytes());
+                StringBuffer sb = new StringBuffer();
+                for (int i = 0; i < array.length; ++i) {
+                    sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1, 3));
                 }
-                hexString.append(hex);
+                return sb.toString();
+            } catch (java.security.NoSuchAlgorithmException e) {
             }
-
-            return hexString.toString();
-        } catch (NoSuchAlgorithmException e) {
+            return null;
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -41,12 +42,11 @@ public class Task3 {
         boolean flag = true;
         for (int j = password.length - 1; j >= 0; j--) {
             if (password[j] != '9') {
-                for (int i = 0; i < ALPHABET.length; i++) {
-                    if (password[j] == ALPHABET[i]) {
-                        password[j] = ALPHABET[i + 1];
-                        flag = false;
-                        break;
-                    }
+                flag = false;
+                if (password[j] == 'z') {
+                    password[j] = '0';
+                } else {
+                    password[j] = (char) (password[j] + 1);
                 }
                 break;
             } else {
@@ -64,25 +64,38 @@ public class Task3 {
 
     public static Map<String, String> passwordEncoding(Map<String, String> passwordMap) {
         Map<String, String> result = new HashMap<>();
-//        BlockingDeque<String> queue = new LinkedBlockingDeque<>(100);
-//        queue.add("");
-        String element = "99";
+        String element = START_PASSWORD;
 
         while (element.length() < MAX_PASSWORD_LENGTH) {
             element = nextPassword(element);
-            int n = element.length();
-            if (element.charAt(n - 1) == 'a' && element.charAt(n - 1 - 1) == 'a'
-                && element.charAt(n - 1 - 1 - 1) == 'a') {
-                //System.out.println(element);
-            }
-            //System.out.println(element);
             String codedString = stringToMD5(element);
             if (passwordMap.containsKey(codedString)) {
                 result.put(passwordMap.get(codedString), element);
-                //System.out.println(element);
             }
         }
 
+        return result;
+    }
+
+    public static Map<String, String> passwordEncodingMultithread(Map<String, String> passwordMap) {
+        Map<String, String> result = new HashMap<>();
+        AtomicReference<String> element = new AtomicReference<>(START_PASSWORD);
+
+        Callable<String> task = () -> {
+            element.set(nextPassword(element.get()));
+            return element.toString();
+        };
+        ExecutorService service = Executors.newFixedThreadPool(THREAD_NUMBER);
+
+        while (element.get().length() < MAX_PASSWORD_LENGTH) {
+            Future future = service.submit(task);
+            String codedString = stringToMD5(element.get());
+            if (passwordMap.containsKey(codedString)) {
+                result.put(passwordMap.get(codedString), element.toString());
+            }
+
+        }
+        service.shutdown();
         return result;
     }
 }
--- src/test/java/edu/hw8/Task3Test.java
+++ src/test/java/edu/hw8/Task3Test.java
@@ -6,7 +6,7 @@ import java.util.HashMap;
 import java.util.Map;
 import static edu.hw8.Task3.Task3.nextPassword;
 import static edu.hw8.Task3.Task3.passwordEncoding;
-import static edu.hw8.Task3.Task3.stringToMD5;
+import static edu.hw8.Task3.Task3.passwordEncodingMultithread;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Task3Test {
@@ -18,10 +18,26 @@ public class Task3Test {
         given.put("d8578edf8458ce06fbc5bb76a58c5ca4", "v.v.belov");
         given.put("482c811da5d5b4bc6d497ffa98491e38", "a.s.ivanov");
         given.put("5f4dcc3b5aa765d61d8327deb882cf99", "k.p.maslov");
-        given.put("47bce5c74f589f4867dbd57e9ca9f808", "o.o.orlov");
-        given.put("9f06243abcb89c70e0c331c61d871fa7", "t.t.testov");
+        given.put("098f6bcd4621d373cade4e832627b4f6", "t.t.testov");
+        Map<String, String> exp = new HashMap<>();
+        exp.put("t.t.testov", "test");
         //act
         var act = passwordEncoding(given);
+
+        assertThat(act).isEqualTo(exp);
+    }
+
+    @Test
+    void passwordEncoding_testWithExecutor() throws NoSuchAlgorithmException {
+        //arrange
+        Map<String, String> given = new HashMap<>();
+        given.put("e10adc3949ba59abbe56e057f20f883e", "a.v.petrov");
+        given.put("d8578edf8458ce06fbc5bb76a58c5ca4", "v.v.belov");
+        given.put("482c811da5d5b4bc6d497ffa98491e38", "a.s.ivanov");
+        given.put("5f4dcc3b5aa765d61d8327deb882cf99", "k.p.maslov");
+        given.put("098f6bcd4621d373cade4e832627b4f6", "t.t.testov");
+        //act
+        var act = passwordEncodingMultithread(given);
 
         for (var i : act.keySet()) {
             System.out.println(i + " " + act.get(i));
@@ -35,6 +51,5 @@ public class Task3Test {
         assertThat(nextPassword("aaa9")).isEqualTo("aaba");
         assertThat(nextPassword("c999")).isEqualTo("daaa");
         assertThat(nextPassword("9999")).isEqualTo("aaaaa");
-        //System.out.println(stringToMD5("tttt"));
     }
 }
