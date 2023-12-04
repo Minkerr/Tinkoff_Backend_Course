f53db49788a6ccb97fa8a591984d86f7c55fbd5e
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-04 10:18:43 +0300

    cs fixes

--- src/main/java/edu/hw8/Task3/Task3.java
+++ src/main/java/edu/hw8/Task3/Task3.java
@@ -3,10 +3,8 @@ package edu.hw8.Task3;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Task3 {
