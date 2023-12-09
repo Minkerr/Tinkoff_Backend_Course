c66c4c641204a7986b5391fe03249b46feda0b72
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-09 19:55:04 +0300

    multithread changes

--- src/main/java/edu/project4/ConcurrentRender.java
+++ src/main/java/edu/project4/ConcurrentRender.java
@@ -8,6 +8,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.stream.Stream;
 
 public class ConcurrentRender implements Render {
     private final int nThreads = 8;
@@ -58,58 +59,69 @@ public class ConcurrentRender implements Render {
         double yMax = 1;
         double xMin = (double) -width / height;
         double xMax = (double) width / height;
-        ExecutorService executor = Executors.newFixedThreadPool(nThreads);
+        ExecutorService executor = Executors.newWorkStealingPool(nThreads);
         CountDownLatch countDownLatch = new CountDownLatch(nThreads);
         Lock lock = new ReentrantLock();
 
         Runnable task = () -> {
-            Point pw = new Point(random(xMin, xMax), random(yMin, yMax));
-            for (int step = 0; step < iterPerSample; ++step) {
-                int i = random(0, numberOfTransformations);
-                double x = coeff.get(i).a() * pw.x() + coeff.get(i).b() * pw.y() + coeff.get(i).c();
-                double y = coeff.get(i).d() * pw.x() + coeff.get(i).e() * pw.y() + coeff.get(i).f();
-                double theta2 = 0.0;
-                Transformation variation = variations.get(random(0, variations.size()));
-                pw = variation.apply(new Point(x, y));
-                for (int s = 0; s < symmetry; theta2 += Math.PI * 2 / symmetry, ++s) {
-                    if (symmetry > 1) {
-                        var pwr = rotate(pw, theta2);
-                        x = pwr.x();
-                        y = pwr.y();
-                    }
-                    if (xMin < x && x < xMax && yMin < y && y < yMax) {
-                        int x1 = width - (int) (((xMax - x) / (xMax - xMin)) * width);
-                        int y1 = height - (int) (((yMax - y) / (yMax - yMin)) * height);
-                        lock.lock();
-                        try {
-                            if (y1 < height && x1 < width) {
-                                if (data[x1][y1] == null) {
-                                    int red = coeff.get(i).red();
-                                    int green = coeff.get(i).green();
-                                    int blue = coeff.get(i).blue();
-                                    data[x1][y1] = new Pixel(red, green, blue, 1);
-                                } else {
-                                    int red = (data[x1][y1].r() + coeff.get(i).red()) / 2;
-                                    int green = (data[x1][y1].g() + coeff.get(i).green()) / 2;
-                                    int blue = (data[x1][y1].b() + coeff.get(i).blue()) / 2;
-                                    int hits = data[x1][y1].hitCount();
-                                    data[x1][y1] = new Pixel(red, green, blue, hits + 1);
+            for (int num = 0; num < samples / nThreads; ++num) {
+                Point pw = new Point(random(xMin, xMax), random(yMin, yMax));
+                for (int step = 0; step < iterPerSample; ++step) {
+                    int i = random(0, numberOfTransformations);
+                    double x = coeff.get(i).a() * pw.x() + coeff.get(i).b() * pw.y() + coeff.get(i).c();
+                    double y = coeff.get(i).d() * pw.x() + coeff.get(i).e() * pw.y() + coeff.get(i).f();
+                    double theta2 = 0.0;
+                    Transformation variation = variations.get(random(0, variations.size()));
+                    pw = variation.apply(new Point(x, y));
+                    for (int s = 0; s < symmetry; theta2 += Math.PI * 2 / symmetry, ++s) {
+                        if (symmetry > 1) {
+                            var pwr = rotate(pw, theta2);
+                            x = pwr.x();
+                            y = pwr.y();
+                        }
+                            if (xMin < x && x < xMax && yMin < y && y < yMax) {
+                                int x1 = width - (int) (((xMax - x) / (xMax - xMin)) * width);
+                                int y1 = height - (int) (((yMax - y) / (yMax - yMin)) * height);
+                                //lock.lock();
+                                synchronized (data){
+                                if (y1 < height && x1 < width) {
+                                    if (data[x1][y1] == null) {
+                                        int red = coeff.get(i).red();
+                                        int green = coeff.get(i).green();
+                                        int blue = coeff.get(i).blue();
+                                        data[x1][y1] = new Pixel(red, green, blue, 1);
+                                    } else {
+                                        int red = (data[x1][y1].r() + coeff.get(i).red()) / 2;
+                                        int green = (data[x1][y1].g() + coeff.get(i).green()) / 2;
+                                        int blue = (data[x1][y1].b() + coeff.get(i).blue()) / 2;
+                                        int hits = data[x1][y1].hitCount();
+                                        data[x1][y1] = new Pixel(red, green, blue, hits + 1);
+                                    }
                                 }
                             }
-                        } finally {
-                            lock.unlock();
+//                            finally {
+//                                lock.unlock();
+//                            }
                         }
-                    }
 
+                    }
                 }
             }
+            countDownLatch.countDown();
         };
 
-        for (int num = 0; num < samples; ++num) {
-            executor.submit(task);
+        List<Runnable> tasks = Stream.generate(() -> task).limit(nThreads).toList();
+        for (var t : tasks) {
+            executor.submit(t);
         }
-        executor.shutdown();
+//            executor.invokeAll(tasks);
+        try {
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
 
+        executor.shutdown();
         FractalImage fractal = new FractalImage(data, width, height);
         if (correction) {
             GammaCorrection gammaCorrection = new GammaCorrection();
