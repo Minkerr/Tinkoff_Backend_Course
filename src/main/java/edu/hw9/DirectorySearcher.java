2ac37a484ee14c5b9482c7dec156153b84d85ea7
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-10 23:29:06 +0300

    add full Task2

--- src/main/java/edu/hw9/DirectorySearcher.java
+++ src/main/java/edu/hw9/DirectorySearcher.java
@@ -9,11 +9,76 @@ import java.util.concurrent.RecursiveTask;
 
 public class DirectorySearcher {
     private List<String> directoriesWithNFiles;
+    private List<String> filesWithCondition;
     private String strPath;
 
     public DirectorySearcher(String str) {
         this.directoriesWithNFiles = new ArrayList<>();
+        this.filesWithCondition = new ArrayList<>();
         this.strPath = str;
+    }
+
+    public List<String> getDirectoriesWithNFiles() {
+        return directoriesWithNFiles;
+    }
+
+    public List<String> getFilesWithCondition() {
+        return filesWithCondition;
+    }
+
+    public String getStrPath() {
+        return strPath;
+    }
+
+    public void findDirectories(File file, int n) {
+        DirectoriesFinder finder = new DirectoriesFinder(file, n);
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.invoke(finder);
+    }
+
+    public void findFiles(File file, int size, String ext) {
+        FileFinder finder = new FileFinder(file, size, ext);
+        ForkJoinPool forkJoinPool = new ForkJoinPool();
+        forkJoinPool.invoke(finder);
+    }
+
+    public class FileFinder extends RecursiveTask<Void> {
+        private File file;
+        private int size;
+        private String extinction;
+
+        public FileFinder(File file, int size, String extinction) {
+            this.file = file;
+            this.size = size;
+            this.extinction = extinction;
+        }
+
+        private String getFileExtension(File file) {
+            String name = file.getName();
+            int lastIndexOf = name.lastIndexOf(".");
+            return name.substring(lastIndexOf + 1);
+        }
+
+        @Override
+        protected Void compute() {
+            File[] files = file.listFiles((f) -> !f.isDirectory());
+            for (var f : files) {
+                if (f != null) {
+                    String thisExt = getFileExtension(f);
+                    long thisSize = f.length();
+                    if (thisSize > size && thisExt.equals(extinction)) {
+                        filesWithCondition.add(f.getPath());
+                    }
+                }
+            }
+            File[] directories = file.listFiles(File::isDirectory);
+            for (var f : directories) {
+                FileFinder newFounder = new FileFinder(f, size, extinction);
+                newFounder.fork();
+                newFounder.join();
+            }
+            return null;
+        }
     }
 
     public class DirectoriesFinder extends RecursiveTask<Void> {
@@ -38,19 +103,5 @@ public class DirectorySearcher {
             }
             return null;
         }
-    }
-
-    public void findDirectories(File file, int n) {
-        DirectoriesFinder finder = new DirectoriesFinder(file, n);
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        forkJoinPool.invoke(finder);
-    }
-
-    public List<String> getDirectoriesWithNFiles() {
-        return directoriesWithNFiles;
-    }
-
-    public String getStrPath() {
-        return strPath;
     }
 }
