f5d809389636b3ef60b130aa0a899ea9c81fe253
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-10 10:36:36 +0300

    checkstyle

--- src/main/java/edu/project4/ConcurrentRender.java
+++ src/main/java/edu/project4/ConcurrentRender.java
@@ -6,8 +6,6 @@ import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 import java.util.stream.Stream;
 
 public class ConcurrentRender implements Render {
