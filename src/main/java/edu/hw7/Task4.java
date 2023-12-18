6c877935ed174e0aa7aca50c5a819856a1dc45f2
Valentin Mirontsev <valentin.mironcev@mail.ru>
2023-12-18 23:50:36 +0300

    csf

--- src/main/java/edu/hw7/Task4.java
+++ src/main/java/edu/hw7/Task4.java
@@ -7,6 +7,12 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.LongAdder;
 
 public class Task4 {
+    private Task4() {
+    }
+
+    private static final int COEFFICIENT_FOR_PI_APPROXIMATING = 4;
+    private static final int N_THREADS = 8;
+
     public static double approximatingPIMonteCarlo(int n) {
         long circ = 0;
         double x;
@@ -18,17 +24,17 @@ public class Task4 {
                 circ++;
             }
         }
-        return 4 * ((double) circ / n);
+        return COEFFICIENT_FOR_PI_APPROXIMATING * ((double) circ / n);
     }
 
     public static double approximatingPIMonteCarloMultithreading(int n) throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(8);
-        CountDownLatch countDownLatch = new CountDownLatch(8);
+        ExecutorService executor = Executors.newFixedThreadPool(N_THREADS);
+        CountDownLatch countDownLatch = new CountDownLatch(N_THREADS);
         LongAdder l = new LongAdder();
         Runnable task = () -> {
             double x;
             double y;
-            for (int i = 0; i < n / 8; i++) {
+            for (int i = 0; i < n / N_THREADS; i++) {
                 x = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
                 y = ThreadLocalRandom.current().nextDouble(-1.0, 1.0);
                 if (x * x + y * y < 1) {
@@ -37,15 +43,15 @@ public class Task4 {
             }
             countDownLatch.countDown();
         };
-        for(int i = 0; i < 8; i++){
+        for (int i = 0; i < N_THREADS; i++) {
             executor.execute(task);
         }
         countDownLatch.await();
         executor.shutdown();
-        return 4 * (l.doubleValue() / n);
+        return COEFFICIENT_FOR_PI_APPROXIMATING * (l.doubleValue() / n);
     }
 
-    public static double calculateInaccuracy(double x){
+    public static double calculateInaccuracy(double x) {
         return Math.abs(x - Math.PI);
     }
 }
